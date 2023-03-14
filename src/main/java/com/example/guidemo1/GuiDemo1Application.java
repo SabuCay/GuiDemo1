08a17a6e3317517a6c5bd34d91957471f7ef633d
SabuCay <cahl@kea.dk>
2023-03-14 09:31:19 +0100

    Test igen

---
+++ src/main/java/com/example/guidemo1/GuiDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.guidemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guidemo1/GuiDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guidemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
